b69c6e9cb953bc0a8731eebf8405fb66b98da6ee
rajalingarao4545 <rajalingarao4545@gmail.com>
2026-05-28 16:50:52 +0530

    create nodes node exporter

--- install_node_exporter.sh
+++ install_node_exporter.sh
@@ -6,13 +6,14 @@ git clone https://github.com/rajalingarao/11.2.create_nodes_node_exporter.git
 
 cd 11.2.create_nodes_node_exporter
 
-echo "*************Installing node_exporter *************"
+echo "*********Installing node_exporter - start*************"
 sudo sh node_exporter/node_exporter.sh || exit 1
-echo "************node_exporter-done**************************"
+echo "**********node_exporter - done**************************"
 
 echo "All installations completed successfully."
 
-echo "**************************************"
+echo "***********node_exporter status - start****************"
 sudo systemctl status node_exporter
+echo "************node_exporter status - end**********"
 sudo netstat -lntp
 echo "**************************************"
